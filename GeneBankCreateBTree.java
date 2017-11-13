44bc98a22dcc74dbdb54036c8d91613b9c61e320
conorcook <conorcook@u.boisestate.edu>
2017-11-13 16:14:15 -0700

    update on btree

--- GeneBankCreateBTree.java
+++ GeneBankCreateBTree.java
@@ -2,7 +2,9 @@
 public class GeneBankCreateBTree {
 
 	public static void main(String[] args) {
-//
+		//parameters
+		//<0/1(no/with Cache)> <degree> <gbk file> <sequence length>
+		//[<cache size>] [<debug level>]
 	}
 
 }
