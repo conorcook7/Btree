e52394e3de64e407ef985a6c55fbd9e24e7d9750
conor <conor@LAPTOP-LJ7F8VBB>
2017-12-02 16:29:54 -0700

    btree update

--- Btree.java
+++ Btree.java
@@ -3,18 +3,19 @@ public class BTree {
 	BTreeNode root;
 	
 	
-	public BTree(){
+	public BTree(boolean cache, int cacheSize){
 		
 	}
 	
 	public class BTreeNode{
 		private int n;			//number of keys stored in the node
 		private int [] keys;	// references to keys stored in node
-		private boolean leaf;
 		private BTreeNode [] Children;	// reference to child nodes 
+		private boolean leaf, root;
+		private int maxKeys, parent, value;
 		
-		public BTreeNode( BTreeNode parent, int maxKeysize, int maxChildrenSize ){
-			
+		public BTreeNode( int value, BTreeNode parent, int maxKeys, int maxChildrenSize ){
+			this.value = value;
 			
 		}
 		
