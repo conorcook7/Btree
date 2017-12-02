e4384fb76f5cb581bf73a34131f141cc2b0887e2
conor <conor@LAPTOP-LJ7F8VBB>
2017-12-02 12:25:09 -0700

    update

--- Btree.java
+++ Btree.java
@@ -1,4 +1,17 @@
 
 public class Btree {
 
+	private treeNode root, current, next;
+
+	private class treeNode {
+		int key;
+
+		public treeNode() {
+
+		}
+
+		public treeNode(int key) {
+
+		}
+	}
 }
