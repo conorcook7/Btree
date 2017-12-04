13cda8487025a73ad65f1d7bd452ff31c0b44f37
Michael Boyle <michaelboyle@u.boisestate.edu>
2017-12-04 10:01:24 +0000

    little start

--- GeneBankSearch.java
+++ GeneBankSearch.java
@@ -2,8 +2,31 @@
 public class GeneBankSearch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if (!((args.length >= 3 && args.length <= 5) && (args[0] == 0 || args[0] == 1))) {
+			printUsage();
+			return;
+		}
+		if (args.length >= 4 && !(args[3] > 0)) {
+			printUsage();
+			return;
+		}
+		if (args.length >= 5 && args[4] != 0) {
+			printUsage();
+			return;
+		}
+		
+		
+		
+		
 
 	}
 
+	
+	static string binName(string title, int sequence, int degree) {
+		return title + ".gbk.btree.data" + sequence + degree;
+	}
+	
+	static void printUsage() {
+		System.out.println("Usage: GeneBankSearch <0/1(no/with Cache)> <btree file> <query file> [<cache size>] [<debug level>]");
+	}
 }
