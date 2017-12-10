7ed45dc4cc010a418793c7e0b67c32a62f97ebd5
Michael Boyle <michaelboyle@u.boisestate.edu>
2017-12-10 13:47:44 +0000

    done

--- src/TestTree.java
+++ src/TestTree.java
@@ -26,13 +26,7 @@ public class TestTree {
 	private final long SEQ_LENGTH_INDEX = DEGREE_INDEX + INT_BYTES;
 	private final long ROOT_INDEX = SEQ_LENGTH_INDEX + INT_BYTES;
 	private final long END_INDEX = ROOT_INDEX + LONG_BYTES;
-	//private final long TREE_START = END_INDEX + LONG_BYTES;
-	
-	private final long PARENT_OFFSET = 0;
-	private final long NUM_CHILDREN_OFFSET = PARENT_OFFSET + LONG_BYTES;
-	private final long NUM_ELEMS_OFFSET = NUM_CHILDREN_OFFSET + INT_BYTES;
-	private final long CHILDREN_START = NUM_ELEMS_OFFSET + INT_BYTES;
-	
+		
 	
 	public TestTree(String path) throws FileNotFoundException {
 		File f = new File(path);
@@ -51,21 +45,24 @@ public class TestTree {
 			e.printStackTrace();
 		}
 		setDegree(degree);
-		Node rootNode = new Node(null, null, null);
-		long rootIndex = writeNode(rootNode);
-		setRoot(rootIndex);
+		setSeqLength(sequenceLength);
+		setRoot(new Node(null, new ArrayList<Node>(), new ArrayList<Sequence>()));
 	}
 	
 	
 	public void insert(String key) {
-		Tuple<Long, Sequence> result = maybeSearch(key);
-		Node resNode = readNode(result.l());
+		Tuple<Node, Sequence> result = maybeSearch(key);
+		Node resNode = result.l();
 		Sequence resSeq = result.r();
 		if (resSeq != null) 
 			resSeq.duplicate();
-		else
-			resNode.add(key);
-			splitChild(result.l());
+		else {
+			ArrayList<Sequence> elems = resNode.getElems();
+			elems.add(keyIndex(elems, key), resSeq);
+			resNode.setElems(elems);
+		}
+		
+			splitChild(resNode);
 	}
 	
 	public Sequence search(String key) {
@@ -73,97 +70,83 @@ public class TestTree {
 	}
 	
 	
-	private Tuple<Long, Sequence> maybeSearch(String key) {
+	private Tuple<Node, Sequence> maybeSearch(String key) {
 		return maybeSearchHelper(key, getRoot());
 	}
 	
-	private Tuple<Long, Sequence> maybeSearchHelper(String key, Long curIndex) {
-		Node curNode = readNode(curIndex);
-		Sequence result = curNode.search(key);
+	private Tuple<Node, Sequence> maybeSearchHelper(String key, Node curNode) {
+		Sequence result = search(key, curNode);
 		if (result != null)
-			return new Tuple<Long, Sequence>(curIndex, result);
-		else if (curNode.isLeaf())
-			return new Tuple<Long, Sequence>(curIndex, null);
+			return new Tuple<Node, Sequence>(curNode, result);
+		else if (isLeaf(curNode))
+			return new Tuple<Node, Sequence>(curNode, null);
 		else
-			return maybeSearchHelper(key, curNode.getChild(curNode.nextIndex(key)));
+			return maybeSearchHelper(key, curNode.getChildren().get(keyIndex(curNode.getElems(), key)));
 	}
 	
-	private void splitChild(Long n) {
-		Node node = readNode(n);
-		if (node.elemNum() >= 2 * getDegree() - 1) {
-			Tuple<ArrayList<Sequence>, ArrayList<Sequence>> seqs = node.split();
-			Tuple<ArrayList<Long>, ArrayList<Long>> children = node.splitChildren();
-			ArrayList<Sequence> left = seqs.l();
-			ArrayList<Sequence> right = seqs.r();
-			ArrayList<Long> leftChildren = children.l();
-			ArrayList<Long> rightChildren = children.r();
-			Sequence middle = node.middle();
-			Node parent = readNode(node.getParent());
+	private void splitChild(Node n) {
+		if (n.getNumElems() >= 2 * getDegree() - 1) {
+			ArrayList<Sequence> elems = n.getElems();
+			ArrayList<Node> children = n.getChildren();
 			
-			parent.add(middle);
-			parent.removeChild(n);
-			int middleIndex = parent.indexOf(middle.getKey());
-			Node newLeft = new Node(node.getParent(), leftChildren, left);
-			Node newRight = new Node(node.getParent(), rightChildren, right);
+			int middleIndex = Math.floorDiv(elems.size(), 2);
+			Sequence middleSeq = elems.get(middleIndex);
+			ArrayList<Sequence> leftElems = (ArrayList<Sequence>) elems.subList(0, middleIndex);
+			ArrayList<Sequence> rightElems = (ArrayList<Sequence>) elems.subList(middleIndex + 1, elems.size() - 1);
+			ArrayList<Node> leftChildren = (ArrayList<Node>) children.subList(0, middleIndex);
+			ArrayList<Node> rightChildren = (ArrayList<Node>) children.subList(middleIndex + 1, children.size() - 1);
+			
+			Node parent;
+			if (getRoot().equals(n)) {
+				ArrayList<Sequence> newRoot = new ArrayList<Sequence>();
+				newRoot.add(middleSeq);
+				parent = new Node(null, newRoot);
+				setRoot(parent);
+			}
+			else 
+				parent = n.getParent();
+			ArrayList<Sequence> parentElems = parent.getElems();
+			ArrayList<Node> parentChildren = parent.getChildren();
+			
+			Node newLeft = new Node(n.getIndex(), parent, leftElems);
+			Node newRight = new Node(parent, rightElems);
+			
+			for (Node ln : leftChildren) 
+				ln.setParent(newLeft);
+			for (Node rn : rightChildren) 
+				rn.setParent(newRight);
+			newLeft.setChildren(leftChildren);
+			newRight.setChildren(rightChildren);
 
-			long lIndex = writeNode(newLeft);
-			long rIndex = writeNode(newRight);
+			parentElems.add(keyIndex(parentElems, middleSeq.getKey()), middleSeq);
+			parentChildren.remove(n);
+			parentChildren.add(keyIndex(parentElems, leftElems.get(0).getKey()), newLeft);
+			parentChildren.add(keyIndex(parentElems, rightElems.get(0).getKey()), newRight);
 			
-			parent.addChild(lIndex, middleIndex);
-			parent.addChild(rIndex, middleIndex + 1);
+			parent.setElems(parentElems);
+			parent.setChildren(parentChildren);
 			
-			writeNode(parent);
-			
-			splitChild(node.getParent());
+			splitChild(parent);
 		}
 	}
 	
-	private Node readNode(Long index) {
-		Long parent = getParent(index);
-		ArrayList<Long> children = new ArrayList<Long>();
-		ArrayList<Sequence> elems = new ArrayList<Sequence>();
-		Node n = null;
-		try {
-			int numChildren = file.readInt();
-			int numElems = file.readInt();
-			for (int i = 0; i < numChildren; i++) {
-				children.add(file.readLong());
-			}
-			for (long i = 0; i < numElems; i++) {
-				String s = "";
-				for (long j = 0; j < seqLength(); j++) {
-					s += file.readChar();
-				}
-				int duplicates = file.readInt();
-				elems.add(new Sequence(s, duplicates));
-			}
-			n = new Node(parent, children, elems);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return n;
-	}
 	
-	private long writeNode(Node n) {
-		return -1;
-	}
-	
-	
-	private Long getRoot() {
-		Long root = null;
+	private Node getRoot() {
+		Node root = null;
 		try {
 			file.seek(ROOT_INDEX);
-			root = file.readLong();
+			long rootIndex = file.readLong();
+			root = new Node(rootIndex);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return root;
 	}
 	
-	private void setRoot(long r) {
+	private void setRoot(Node n) {
 		try {
 			file.seek(ROOT_INDEX);
-			file.writeLong(r);
+			file.writeLong(n.getIndex());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -229,64 +212,208 @@ public class TestTree {
 		}
 	}
 	
-	
-	private Long getParent(long index) {
-		Long parent = null;
-		try {
-			file.seek(index + PARENT_OFFSET);
-			parent = file.readLong();
-		} catch (IOException e) {
-			e.printStackTrace();
+	private int keyIndex(ArrayList<Sequence> elems, String key) {
+		for (int i = 0; i < elems.size(); i++) {
+			if (key.compareTo(elems.get(i).getKey()) < 0)
+				return i;
 		}
-		return parent;
+		return elems.size();
 	}
 	
-	private void setParent(long index, long parent) {
-		try {
-			file.seek(index + PARENT_OFFSET);
-			file.writeLong(parent);
-		} catch (IOException e) {
-			e.printStackTrace();
+	private Sequence search(String key, Node n) {
+		ArrayList<Sequence> elems = n.getElems();
+		for (Sequence elem : elems) {
+			if (elem.getKey() == key)
+				return elem;
+		}
+		return null;
+	}
+	
+	private boolean isLeaf(Node n) {
+		ArrayList<Node> children = n.getChildren();
+		return children.size() > 0;
+	}
+	
+	
+	private class Node {
+		
+		
+		long index;
+		
+		private final long PARENT_OFFSET = 0;
+		private final long NUM_CHILDREN_OFFSET = PARENT_OFFSET + LONG_BYTES;
+		private final long NUM_ELEMS_OFFSET = NUM_CHILDREN_OFFSET + INT_BYTES;
+		private final long CHILDREN_START = NUM_ELEMS_OFFSET + INT_BYTES;
+		private final long ELEMS_START = CHILDREN_START + LONG_BYTES * 2 * getDegree();
+		private final long END_OFFSET = ELEMS_START + (CHAR_BYTES + INT_BYTES) * 2 * getDegree();
+
+		
+		private Node(long index) {
+			this.index = index;
+			if (index == getEnd())
+				setEnd(getEnd() + END_OFFSET);
+		}
+		
+		private Node(long index, Node parent, ArrayList<Sequence> elems) {
+			this.index = index;
+			this.setParent(parent);
+			this.setElems(elems);
+			setEnd(getEnd() + END_OFFSET);
+			if (index == getEnd())
+				setEnd(getEnd() + END_OFFSET);
+		}
+		
+		private Node(Node parent, ArrayList<Sequence> elems) {
+			this.index = getEnd();
+			this.setParent(parent);
+			this.setElems(elems);
+			setEnd(getEnd() + END_OFFSET);
+		}
+		
+		private Node(Node parent, ArrayList<Node> children, ArrayList<Sequence> elems) {
+			this.index = getEnd();
+			this.setParent(parent);
+			this.setChildren(children);
+			this.setElems(elems);
+			setEnd(getEnd() + END_OFFSET);
+		}
+		
+		private Node(long index, Node parent, ArrayList<Node> children, ArrayList<Sequence> elems) {
+			this.index = index;
+			this.setParent(parent);
+			this.setChildren(children);
+			this.setElems(elems);
+			setEnd(getEnd() + END_OFFSET);
+		}
+		
+		
+		private Node getParent() {
+			Long parentIndex = null;
+			try {
+				file.seek(index + PARENT_OFFSET);
+				parentIndex = file.readLong();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return new Node(parentIndex);
+		}
+		
+		private void setParent(Node parent) {
+			try {
+				file.seek(index + PARENT_OFFSET);
+				file.writeLong(parent.getIndex());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		private int getNumChildren() {
+			Integer numChildren = null;
+			try {
+				file.seek(index + NUM_CHILDREN_OFFSET);
+				numChildren = file.readInt();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return numChildren;
+		}
+		
+		private void setNumChildren(int num) {
+			try {
+				file.seek(index + NUM_CHILDREN_OFFSET);
+				file.writeInt(num);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		private int getNumElems() {
+			Integer numElems = null;
+			try {
+				file.seek(index + NUM_ELEMS_OFFSET);
+				numElems = file.readInt();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return numElems;
+
+		}
+		
+		private void setNumElems(int num) {
+			try {
+				file.seek(index + NUM_ELEMS_OFFSET);
+				file.writeInt(num);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		private ArrayList<Node> getChildren() {
+			ArrayList<Node> children = new ArrayList<Node>();
+			try {
+				int numChildren = getNumChildren();
+				file.seek(index + CHILDREN_START);
+				for (int i = 0; i < numChildren; i++) {
+					children.add(new Node(file.readLong()));
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return children;
+		}
+		
+		private void setChildren(ArrayList<Node> children) {
+			try {
+				file.seek(index + CHILDREN_START);
+				for (int i = 0; i < children.size(); i++) {
+					file.writeLong(children.get(i).getIndex());
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			setNumChildren(children.size());			
+		}
+		
+		private ArrayList<Sequence> getElems() {
+			ArrayList<Sequence> elems = new ArrayList<Sequence>();
+			try {
+				int numElems = getNumElems();
+				file.seek(index + ELEMS_START);
+				for (int i = 0; i < numElems; i++) {
+					String s = "";
+					for (int j = 0; j < seqLength(); j++) {
+						s += file.readChar();
+					}
+					int duplicates = file.readInt();
+					elems.add(new Sequence(s, duplicates));
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return elems;
+		}
+		
+		private void setElems(ArrayList<Sequence> elems) {
+			try {
+				file.seek(index + ELEMS_START);
+				for (int i = 0; i < elems.size(); i++) {
+					Sequence elem = elems.get(i);
+					file.writeChars(elem.getKey());
+					file.writeInt(elem.getDuplicates());
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			setNumElems(elems.size());
+		}
+		
+		private long getIndex() {
+			return this.index;
 		}
 
-	}
-	
-	private ArrayList<Long> getChildren(long index) {
-		ArrayList<Long> children = new ArrayList<Long>();
-		try {
-			file.seek(index + CHILDREN_START);
-			for (int i = 0; i < getNumChildren(index); i++) {
-				children.add(file.readLong());
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return children;
-	}
-	
-	private void setChildren(long index, ArrayList<Long> children) {
-		setNumChildren(index, children.size());
-		try {
-			file.seek(index + CHILDREN_START);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		
 	}
 	
-	private int getNumChildren(long index) {
-		return -1;
-	}
-	
-	private void setNumChildren(long index, int num) {
-		
-	}
-	
-	
-	
-	private boolean isRoot(long n) {
-		return n == getRoot();
-	}
 
 
 
@@ -301,4 +428,3 @@ public class TestTree {
 
 
 
-//hello
