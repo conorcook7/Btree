a36be43dad2a8b4cb81f3ae45e93610b263dd6c7
MichaelBoyle <michaelboyle@onyxnode77.boisestate.edu>
2017-12-10 16:47:40 -0700

    hey

--- src/GeneBankCreateBTree.java
+++ src/GeneBankCreateBTree.java
@@ -1,176 +1,117 @@
 import java.util.*;
 import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
 
 /**
  * @author Zachary Garner
  */
 public class GeneBankCreateBTree {
-	
-/**
- * 
- */
-	public static void main(String[] args){
-		
-		if( args.length < 3 || args.length > 5){
-			System.out.println("Input should be in the following format:" + "\n" +
-					" <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]"
-					+ "\n The sequence length needs to be beween 0 and 31.");
-			System.exit(1);
-					
-		}
-		try{
-			parseCommandLineArgs(args);
-		}catch( Exception e ){
-			e.printStackTrace();
-			System.exit(1);
-		}
-	}
-	
+
 	/**
-	 * Checks command line arguments and prints a message if the arguments are not correct.
+	 * 
 	 */
-	public static void parseCommandLineArgs(String[] args)throws Exception{
-		
+	public static void main(String[] args) {
+
 		int degree = 0;
-		File myFile = null;
+		String gbkFile = null;
 		int seqLength = 0;
-		int cacheSize = 0;
-		int debugLvl = 0;
-		
-		for (int i = 0; i < args.length; i++){
-			switch(i){
-			
-			case 0: 
-				try{
-					degree = Integer.parseInt(args[i]);
-				}catch(NumberFormatException e){
-					e.printStackTrace();
-					System.exit(1);
-				}	
-			break;
-			
-			case 1:
-				try{
-					myFile = new File(args[i]);
-					if( ! myFile.exists() ) throw new FileNotFoundException("The File " + args[i] + " does not exist."); 
-				}
-				catch(FileNotFoundException e){
-					e.printStackTrace();
-				}
-			break;
-			
-			case 2:
-				try{
-					seqLength = Integer.parseInt(args[i]);
-					if(seqLength > 31 || seqLength < 0){
-						System.out.println("The sequence length needs to be between 0 and 31.\n");
-						System.exit(1);
-						}
-				}catch(NumberFormatException e){
-					e.printStackTrace();
-				}
-			break;
-			
-			case 3: 
-				try{
-					cacheSize = Integer.parseInt(args[i]);
-				}catch(NumberFormatException e){
-					e.printStackTrace();
-				}
-			break;
-			
-			case 4:
-				try{
-					debugLvl = Integer.parseInt(args[i]);
-				}catch(NumberFormatException e){
-					e.printStackTrace();
-				}	
-				if(debugLvl == 0){
-					System.out.println("no debug");
-				}else if(debugLvl == 1){
-					System.out.println("with debug");
-				}else{
-					System.out.println("arg 1 incompatible");
-					System.exit(1);
-					}
-				break;
-			}
-			
+		boolean debugLvl = false;
+
+		try {
+			degree = Integer.parseInt(args[0]);
+			gbkFile = args[1];
+			seqLength = Integer.parseInt(args[2]);
+			if (args.length == 4)
+				debugLvl = args[3] == "1";
+			parseFile(degree, gbkFile, seqLength, debugLvl);
+		} catch (FileAlreadyExistsException e) {
+			System.out.println("bin associated with gbk already exists");
+		} catch (Exception e) {
+			printUsage();
+			return;
 		}
-		
-		System.out.println("end of parse");
-		
-		parseFile( degree, myFile, seqLength, cacheSize, debugLvl);
-		
+
 	}
+
 
 	/**
 	 * @param cache
 	 * @param degree
-	 * @param file 
+	 * @param file
 	 * @param seqLength
 	 * @param cacheSize
 	 * @param debugLvl
+	 * @throws FileAlreadyExistsException 
 	 */
-	public static void parseFile( int degree, File file, int seqLength, int cacheSize, int debugLvl)throws Exception{
-		
-		
-		TestTree tree = new TestTree("thefilewewillsaveto.txt", degree, seqLength);
+	public static void parseFile(int degree, String file, int seqLength, boolean debugLvl) throws FileAlreadyExistsException {
 
+		String binName = file + ".btree.data." + seqLength + "." + degree;
+		File gbkFile = new File(file);
+
+		TestTree tree = null;
+		tree = new TestTree(binName, degree, seqLength);
+		System.out.println("construct tree");
+		System.out.println("bin associated with gbk already exists");
+
+		Scanner scanFile = null;
+		try {
+			scanFile = new Scanner(gbkFile);
+		} catch (FileNotFoundException e) {
+			System.out.println("gbk file not found");
+			e.printStackTrace();
+		}
 		
-		
-		try{
-		Scanner scanFile = new Scanner(file);
 		StringBuilder SB;
 		Scanner otherScanner;
-		
+
 		String curVal = scanFile.next();
-		
-		while (scanFile.hasNext()){
-			if( curVal.equals("ORIGIN") ) { 	//finds the key origin indicating the beginning of the dna sequence 
+
+		while (scanFile.hasNext()) {
+			if (curVal.equals("ORIGIN")) { // finds the key origin indicating the beginning of the dna sequence
 				System.out.println("Origin found");
 				SB = new StringBuilder();
-				
-				while(!curVal.equals("//") && !curVal.equals("N")){
-					scanFile.next(); 	// removes the in at the beginning of each line in the sequence
-					
+
+				while (!curVal.equals("//") && !curVal.equals("N")) {
+					scanFile.next(); // removes the in at the beginning of each line in the sequence
+
 					SB.append(scanFile.nextLine()); // adds line to the string representing the DNA sequence
 					otherScanner = scanFile;
 					curVal = otherScanner.next();
-					
+
 				}
 				String dnaSeq = new String(SB);
-				dnaSeq = dnaSeq.replaceAll("\\s+",""); // removes spaces from the string that represents a DNA sequence
-				parseToSubSequence(dnaSeq, seqLength, degree, tree);
+				dnaSeq = dnaSeq.replaceAll("\\s+", ""); // removes spaces from the string that represents a DNA
+														// sequence
+				parseToSubSequence(dnaSeq, degree, tree);
 			}
-			if(scanFile.hasNext()) curVal = scanFile.next();
+			if (scanFile.hasNext())
+				curVal = scanFile.next();
 		}
-		
-		
+
 		scanFile.close();
-		}catch(FileNotFoundException e){
-		}
 	}
-	
-	
+
 	/**
 	 * @param seq
 	 * @param seqLength
 	 * 
 	 */
-	public static void parseToSubSequence(String seq, int seqLength, int degree, TestTree tree){
-		
+	public static void parseToSubSequence(String seq, int degree, TestTree tree) {
+
 		StringBuilder SB = new StringBuilder();
-		
-		for(int i = 0; i < seq.length() - seqLength; i++ ){
-			for(int j = i; j < i + seqLength; j++ ){
+
+		for (int i = 0; i < seq.length(); i++) {
+			for (int j = i; j < i + seq.length(); j++) {
 				SB.append(seq.charAt(j));
 			}
 			System.out.println(SB.toString());
 			tree.insert(SB.toString());
 			SB.setLength(0);
 		}
-		
+	}
+
+	static void printUsage() {
+		System.out.println("Usage: GeneBankCreatBTree <degree> <gbk file> <sequence length> [0/1<debug level>]");
 	}
 
 }
